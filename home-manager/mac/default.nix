96a594dc28549e36145083ccf0d8cd3368443bb6
RXHAP <83570263+RXHAP@users.noreply.github.com>
2026-07-08 17:00:12 +0900

    feat: add homebrew and xquartz

--- home-manager/mac/default.nix
+++ home-manager/mac/default.nix
@@ -8,4 +8,15 @@ in
 
   home.username = "rxhap";
   home.homeDirectory = "/Users/rxhap";
+
+  programs.zsh.initContent = ''
+    export HOMEBREW_PREFIX="/opt/homebrew";
+    export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+    export HOMEBREW_REPOSITORY="/opt/homebrew";
+    fpath[1,0]="/opt/homebrew/share/zsh/site-functions";
+    export FPATH;
+    eval "$(/usr/bin/env PATH_HELPER_ROOT="/opt/homebrew" /usr/libexec/path_helper -s)"
+    [ -z "''${MANPATH-}" ] || export MANPATH=":''${MANPATH#:}";
+    export INFOPATH="/opt/homebrew/share/info:''${INFOPATH:-}";
+  '';
 }
